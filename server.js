45e37325f554c0ba7d14a2a84913fa4a989dccbf
Rebecca Vest <vestre@Rebeccas-MacBook-Pro.local>
2017-04-05 15:11:58 -0600

    Bug fixes to show language & os

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require("express");
-var platform = require("platform");
+var useragent = require("useragent");
 var app = express();
 
 app.get("/", function (req, res) {
-    res.json({ ip: req.ip, language: req.acceptsLanguages, software: + platform.os });
+    var agent = useragent.parse(req.headers['user-agent']).os.toString();
+    res.json({ ip: req.ip, language: req.acceptsLanguages()[0], software: agent.os.toString()});
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, function () {
+app.listen(PORT, "127.0.0.1", function () {
   console.log("App listening on port " + PORT);
 });
